a036a3892e07b6595bfdd6ada4f2cafd67edb222
SpiNoice01 <fadhilaagil72@gmail.com>
2025-01-20 19:14:18 +0700

    intent bermaingame

--- Visign/app/src/main/java/com/example/visign/MainActivity.kt
+++ Visign/app/src/main/java/com/example/visign/MainActivity.kt
@@ -1,20 +1,18 @@
 package com.example.visign
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        findViewById<View>(R.id.tombolplay).setOnClickListener {
+            val intent = Intent(this, PlayActivity::class.java)
+            startActivity(intent)
         }
     }
 }
