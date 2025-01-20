ea30596e61a6578c09e5f955af50e9347083042b
SpiNoice01 <fadhilaagil72@gmail.com>
2025-01-20 09:21:41 +0700

    first commit, splashscreen & homescreen

---
+++ Visign/app/src/main/java/com/example/visign/SplashScreenActivity.kt
@@ -0,0 +1,21 @@
+package com.example.visign
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 3000) // Splash screen delay 3 detik
+    }
+}
